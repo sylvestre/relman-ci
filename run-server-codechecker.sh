54a4dc968efbe82ae78790a11566a7b5f6eef311
Sylvestre Ledru <sylvestre@debian.org>
2018-01-28 09:26:11 +0100

    Update of the command line to start the server

--- run-server-codechecker.sh
+++ run-server-codechecker.sh
@@ -2,6 +2,7 @@ source ~/workspace/update-codechecker/venv/bin/activate
 export PATH=/var/lib/jenkins/workspace/update-codechecker/build/CodeChecker/bin:$PATH
 #CodeChecker server --postgresql --dbusername codechecker_user -u /var/lib/jenkins/workspace/firefox-codechecker/suppress.txt  --not-host-only
 #CodeChecker server -w /var/lib/jenkins/.codechecker --postgresql --dbname codechecker --dbport 5432 --dbusername codechecker_user -u /var/lib/jenkins/workspace/firefox-codechecker/suppress.txt  --not-host-only
-CodeChecker server -w /var/lib/jenkins/.codechecker --postgresql --dbname codechecker_config --dbport 5432 --dbusername codechecker_user   --not-host-only --verbose debug
+CodeChecker server -w /var/lib/jenkins/.codechecker --postgresql --dbname codechecker_config --dbport 5432 --dbusername codechecker_user   --not-host-only
+# --verbose debug
 
 
