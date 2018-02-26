db0ea545c8ed8e8ca46a7464dbcd699302c5a4e4
root <root@irill4.inria.fr>
2018-02-26 08:46:23 +0100

    add --insecure to curl

--- opt/upload-to-coverity-fx.sh
+++ opt/upload-to-coverity-fx.sh
@@ -1,2 +1,2 @@
-curl --form token=$(cat /opt/fx-pass.txt)   --form email=sylvestre@debian.org   --form file=@cov-build.tar.gz   --form version="$BUILD_ID"   --form description="Description"   https://scan.coverity.com/builds?project=Firefox
+curl --insecure --form token=$(cat /opt/fx-pass.txt)   --form email=sylvestre@debian.org   --form file=@cov-build.tar.gz   --form version="$BUILD_ID"   --form description="Description"   https://scan.coverity.com/builds?project=Firefox
 
--- opt/upload-to-coverity-gcc.sh
+++ opt/upload-to-coverity-gcc.sh
@@ -1,2 +1,2 @@
-curl --form token=$(cat /opt/gcc-pass.txt)   --form email=sylvestre@debian.org   --form file=@cov-build.tar.gz   --form version="$BUILD_ID"   --form description=""   https://scan.coverity.com/builds?project=gcc
+curl --insecure --form token=$(cat /opt/gcc-pass.txt)   --form email=sylvestre@debian.org   --form file=@cov-build.tar.gz   --form version="$BUILD_ID"   --form description=""   https://scan.coverity.com/builds?project=gcc
 
