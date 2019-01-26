27fe094423b7c29958008808b08a2e3e1c90d65f
Sylvestre Ledru <sylvestre@debian.org>
2019-01-26 10:49:30 +0000

    update of the codechecker run script

--- run-server-codechecker.sh
+++ run-server-codechecker.sh
@@ -1,6 +1,5 @@
 source ~/workspace/update-codechecker/venv/bin/activate
 export PATH=/var/lib/jenkins/workspace/update-codechecker/build/CodeChecker/bin:$PATH
-CodeChecker server -w /var/lib/jenkins/.codechecker --postgresql --dbname codechecker_config --dbport 5432 --dbusername codechecker_user
-#   --not-host-only
+CodeChecker server -w /var/lib/jenkins/.codechecker --postgresql --dbname codechecker_config --dbport 5432 --dbusername codechecker_user   --not-host-only
 
 
